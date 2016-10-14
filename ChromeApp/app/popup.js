84da914732cd6480d107a7b61b67ba07a072470b
quinn pan <pjkui@qq.com>
2016-10-15 00:42:29 +0800

    GetRepeatItems and fixed bug in sort method

--- ChromeApp/app/popup.js
+++ ChromeApp/app/popup.js
@@ -91,9 +91,11 @@ var BookmarkManager = {
     },
 
     DeleteItemByBookmarkNode: function (_bookmarkNode) {
+
         var i = 0;
         var len = this.bookmarkData.length;
         var deleteItemCount = 0;
+        this.repeatItems.length = 0;
         for (; i < len; ++i) {
             var it = this.bookmarkData[i];
             if (it.url == _bookmarkNode.url && it.title == _bookmarkNode.title) {
@@ -105,7 +107,28 @@ var BookmarkManager = {
         return deleteItemCount;
     },
 
-    RepeatItemCount: function () {
+    GetRepeatItems: function () {
+        this.SortByUrl();
+        var len = this.bookmarkCount();
+        var i = 0;
+        var temp = { url: "" };
+        var firstTime = true;
+        for (; i < len; ++i) {
+            if (this.bookmarkData[i]) {
+                if (temp.url && this.bookmarkData[i].url == temp.url) {
+                    if (firstTime) {
+                        this.repeatItems.push(temp);
+                        firstTime = false;
+                    }
+                    this.repeatItems.push(this.bookmarkData[i]);
+                }
+                else {
+                    temp = this.bookmarkData[i];
+                    firstTime = true;
+                }
+            }
+        }
+        console.log("repeat items count is " + this.repeatItems.length);
 
     },
     FindItemsByUrl: function (_url) {
@@ -138,6 +161,14 @@ var BookmarkManager = {
         this.bookmarkData.sort(function (a, b) {
             var urlA = a.url;
             var urlB = b.url;
+
+            if (urlA == undefined) {
+                return 1;
+            }
+            if (urlB == undefined) {
+                return -1;
+            }
+
             if (urlA > urlB) {
                 return 1;
             }
@@ -151,6 +182,13 @@ var BookmarkManager = {
         this.bookmarkData.sort(function (a, b) {
             var urlA = a.url;
             var urlB = b.url;
+            if (urlA == undefined) {
+                return -1;
+            }
+            if (urlB == undefined) {
+                return 1;
+            }
+
             if (urlA > urlB) {
                 return -1;
             }
@@ -190,7 +228,9 @@ var BookmarkManager = {
     SortByAddTime: function () {
         this.SortByAddTimeAsc();
     },
+
     bookmarkData: new Array,
+    repeatItems: new Array,
 
     bookmarkCount: function () {
         //return bookmark Count
@@ -208,24 +248,6 @@ var BookmarkManager = {
     //sync data from server
     SyncFromServer: function () { },
 }
-function findBookmarkElementByTitle(title) {
-    var result = bookmarkData.find(function (item) {
-        if (item.title == title) {
-            return item;
-        }
-        return null;
-    });
-}
-
-function findBookmarkElementByUrl(url) {
-    var result = bookmarkData.find(function (item) {
-        if (item.url == url) {
-            return item;
-        }
-        return null;
-    });
-}
-
 
 // Search the bookmarks when entering the search keyword.
 $(function () {
@@ -233,7 +255,80 @@ $(function () {
         $('#bookmarks').empty();
         dumpBookmarks($('#search').val());
     });
+    $("#GetRepetItem").click(function () {
+        GetRepeatItems();
+    });
 });
+function GetRepeatItems() {
+    BookmarkManager.GetRepeatItems();
+
+    var list = $('<ul>');
+    for (var i = 0; i < BookmarkManager.repeatItems.length; ++i) {
+        var li = $('<li>');
+        li.append("<b>" + BookmarkManager.repeatItems[i].title + "</b><span>" + BookmarkManager.repeatItems[i].url + "</span>");
+        var options = $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
+'href="#">Delete</a>]</span>');
+        var edit = $('<table><tr><td>Name</td><td>' +
+                '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
+                '</td></tr></table>');
+        li.hover(function () {
+            li.append(options);
+            $('#deletelink').click(function () {
+                $('#deletedialog').empty().dialog({
+                    closeOnEscape: true,
+                    autoOpen: false,
+                    title: 'Confirm Deletion',
+                    resizable: false,
+                    height: 140,
+                    modal: true,
+                    show: 'slide',
+                    overlay: {
+                        backgroundColor: '#000',
+                        opacity: 0.5
+                    },
+                    buttons: {
+                        'Yes, Delete It!': function () {
+                            chrome.bookmarks.remove(String(BookmarkManager.repeatItems[i].id));
+                            span.parent().remove();
+                            $(this).dialog('destroy');
+                        },
+                        Cancel: function () {
+                            $(this).dialog('destroy');
+                        }
+                    }
+                }).dialog('open');
+            });
+            $('#editlink').click(function () {
+                edit.val(BookmarkManager.repeatItems[i].title);
+                $('#editdialog').empty().append(edit).dialog({
+                    autoOpen: false,
+                    closeOnEscape: true,
+                    title: 'Edit Bookmark',
+                    modal: true,
+                    show: 'slide',
+                    buttons: {
+                        'Save': function () {
+                            chrome.bookmarks.update(String(BookmarkManager.repeatItems[i].id), {
+                                title: edit.val()
+                            });
+                            anchor.text(edit.val());
+                            options.show();
+                            $(this).dialog('destroy');
+                        },
+                        'Cancel': function () {
+                            $(this).dialog('destroy');
+                        }
+                    }
+                }).dialog('open');
+            });
+            options.fadeIn();
+        }, function () {
+            options.remove();
+        });
+        list.append(li);
+    }
+    $('#bookmarks').empty().append(list);
+}
 // Traverse the bookmark tree, and print the folder and nodes.
 
 function dumpBookmarks(query) {
